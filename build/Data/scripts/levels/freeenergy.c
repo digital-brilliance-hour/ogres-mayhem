fde3f4c11d1d57eff2ce43a8a3413c8bb817e884
DBH Coder <reanimixtv@gmail.com>
2022-12-07 21:24:25 -0500

    start of work

--- build/Data/scripts/levels/freeenergy.c
+++ build/Data/scripts/levels/freeenergy.c
@@ -13,6 +13,40 @@ void main() {
 	            lvup(i);
 	            changeentityproperty(p, "health", getentityproperty(p, "maxhealth"));
 	            changeentityproperty(p, "mp", getentityproperty(p, "maxmp"));
+                executeanimation(p, openborconstant("ANI_RUN"));
+                void iUp = playerkeys(i, 1, "moveup"); // New key status of "Up"
+                void iDown = playerkeys(i, 1, "movedown"); // New key status of "Down"
+                void iLeft = playerkeys(i, 1, "moveleft"); // New key status of "Left"
+                void iRight = playerkeys(i, 1, "moveright"); // New key status of "Right"
+                void iUpR = playerkeys(i, 2, "moveup"); // New key status of "Up"
+                void iDownR = playerkeys(i, 2, "movedown"); // New key status of "Down"
+                void iLeftR = playerkeys(i, 2, "moveleft"); // New key status of "Left"
+                void iRightR = playerkeys(i, 2, "moveright"); // New key status of "Right"
+                int Vx, Vy, Vz;
+                Vx = 0;
+                Vy = 0;
+                Vz = 0;
+                if(iUp){
+                    //changeentityproperty(p, "velocity", 0, -2, 0);
+                    Vy = -2;
+                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
+                }
+                if(iUpR) {
+                    //changeentityproperty(p, "velocity", 0, 0, 0);
+                    Vy = 0;
+                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
+                }
+                if(iDown){
+                    ///changeentityproperty(p, "velocity", 0, 2, 0);
+                    Vy = 2;
+                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
+                }
+                if(iDownR) {
+                    //changeentityproperty(p, "velocity", 0, 0, 0);
+                    Vy = 0;
+                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
+                }
+                
             }
         }
     }
