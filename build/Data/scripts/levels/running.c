2468db1b57649b34849fd53abd4b6e111a006c59
JingoBurch <robynburch88@gmail.com>
2022-12-12 17:58:56 -0500

    dbh-244

---
+++ build/Data/scripts/levels/running.c
@@ -0,0 +1,25 @@
+
+void main() {
+	
+    void p;
+    int i, hp, mp, lv, num;
+    for(i=0; i<4; i++){
+        p = getplayerproperty(i, "entity");
+        num = i+1;
+        if(p){
+            if("Travel"!=getentityproperty(p, "model")) {
+	            changeplayerproperty(i, "score", 200000);
+	            lvup(i);
+    }
+
+    int  iMax       = openborvariant("ent_max");            //Entity count.
+    int  iEntity;                                           //Loop counter.
+    void vEntity;                                           //Target entity.
+    for(iEntity=0; iEntity<iMax; iEntity++)
+    {   
+        vEntity = getentity(iEntity); 
+        if(getentityproperty(vEntity, "type") == openborconstant("TYPE_ENEMY")) {
+        	changeentityproperty(vEntity, "health", getentityproperty(vEntity, "maxhealth"));
+        }	
+    } 
+}
