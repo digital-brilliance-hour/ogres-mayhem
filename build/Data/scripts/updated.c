04613918d3ef5b2eb35b46be8857056174b80b6f
JingoBurch <robynburch88@gmail.com>
2022-12-14 17:58:21 -0500

    I updated the version name to 0.9.5 - beta.

--- build/Data/scripts/updated.c
+++ build/Data/scripts/updated.c
@@ -76,7 +76,7 @@ void mainLoop()
     //highScoreMusic();
   }
   if(SYS_INMENU || SYS_INTITLE){
-      drawstring(400,10,1,"Version: 0.9.5 - beta");
+      drawstring(350,10,1,"Version: 0.9.5 - beta");
   } 
 
 	if(openborvariant("in_selectscreen")==1){
