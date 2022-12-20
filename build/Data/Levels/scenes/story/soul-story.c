dc8d8ad27d14c97494822312d098783d31caec4d
DBH Coder <reanimixtv@gmail.com>
2022-12-20 16:24:37 -0500

    stage8/bonus/sora

---
+++ build/Data/Levels/scenes/story/soul-story.c
@@ -0,0 +1,64 @@
+#include "data/scripts/story/story_scroll.c"
+
+void main()
+{	
+	scrollText("soul-story.txt",91,0.3,"back1",0.09);
+	drawstring(0360,253,1000,"Press Start To Skip");
+  
+	float y = getlayerproperty("background", 0, "zoffset");
+	int ypos = getlocalvar("stagey");
+
+	void p;
+    int i, hp, mp, lv, num;
+    for(i=0; i<4; i++){
+        p = getplayerproperty(i, "entity");
+        num = i+1;
+        if(p){
+       		changeentityproperty(p, "gfxshadow", 1);
+       		changeentityproperty(p, "mp", 0);
+            executeanimation(p, openborconstant("ANI_IDLE")); 
+		    changedrawmethod(p, "enabled", 1);
+		    changedrawmethod(p, "alpha", 6);  
+		    changedrawmethod(p, "channelb", 0);
+		    changedrawmethod(p, "channelg", 0);
+		    changedrawmethod(p, "channelr", 0);
+        }
+    }
+	/*log(ypos);
+	log(" - ypos. ");
+	log(y);
+	log(" - y. ");
+	if(ypos < 250) {
+		y += 1;
+		ypos += 1;
+		setlocalvar("stagey", ypos);
+	}
+	else {
+
+	    void blackMask = getglobalvar("Title2"); 
+	    float opacity = getlocalvar("opacitynum");
+	    if(opacity < 150) {
+	    	opacity += 1;
+	    	setlocalvar("opacitynum", opacity);
+	    	log(opacity);
+	    }
+	    else {
+	    	opacity = 150;
+	    }
+	      
+	    changedrawmethod(NULL(), "enabled", 1);
+	    changedrawmethod(NULL(), "alpha", 6);  
+	    changedrawmethod(NULL(), "channelb", opacity);
+	    changedrawmethod(NULL(), "channelg", opacity);
+	    changedrawmethod(NULL(), "channelr", opacity);
+	    drawsprite(blackMask, 0, 0, -1); 
+	}
+	changelayerproperty("background", 0, "zratio", 0.1);
+	changelayerproperty("background", 0, "zoffset", y); */
+}
+
+void oncreate() {
+	setlocalvar("stagey", 0);
+	setlocalvar("opacitynum", 0);
+	changedrawmethod(NULL(), "reset", 1);
+}
--- build/Data/scripts/levels/running.c
+++ build/Data/scripts/levels/running.c
@@ -1,6 +1,8 @@
 void main() {
 
-    
+    log("Current X value "+openborvariant("xpos")+".  ");
+    int lwidth = openborvariant("levelwidth");
+    int lpos = openborvariant("xpos");
     void p;
     int i, hp, mp, lv, num;
     for(i=0; i<4; i++){
@@ -8,7 +10,67 @@ void main() {
         num = i+1;
         if(p){
             if("Travel"!=getentityproperty(p, "model")) {
-                executeanimation(p, openborconstant("ANI_RUN"));
+                int xpos = getentityproperty(p, "x");
+                int plives = getplayerproperty(i, "lives");
+                changeentityproperty(p, "mp", 0);
+                if(xpos >= lwidth-120) {
+                    void vEntity;                                       //Target entity placeholder.
+                    int  iEntity; 
+                    char iName;                                         //Entity Name.
+                    int  iMax      = openborvariant("count_entities");  //Entity count.
+
+                    //Enumerate and loop through entity collection.
+                    for(iEntity=0; iEntity<iMax; iEntity++){
+                      vEntity = getentity(iEntity);                 //Get target entity from current loop.
+                      iName   = getentityproperty(vEntity, "name"); //Get target name.
+                      
+                      if(iName == "bLargeFireball"){ //Same alias?
+                        changeentityproperty(vEntity, "candamage", openborconstant("TYPE_ENEMY"));
+                      }
+                    }
+                    executeanimation(p, openborconstant("ANI_IDLE"));
+                    //jumptobranch("", 1);
+                    if(getlocalvar("removing"+i) == NULL()) {
+                        void subent;
+                        loadmodel("pspawn"); // name of the entity to be loaded
+                        clearspawnentry(); // clean the spawn entry
+                        setspawnentry("name", "pspawn"); // define the entity to be spawn
+                        setspawnentry("coords", -1,-1,200); // set the position of the entity
+                        subent=spawn();  //  spawn the entity
+                        bindentity(subent, p, 0, 0, 0, 0, 0);
+                        changeentityproperty(subent, "parent", p); //Set caller as parent.
+                        changeentityproperty(subent, "setlayer", 200);
+                        changeentityproperty(subent, "position", 0, 0, 200);
+                        setentityvar(subent, "jumpTo", "Ending");
+                        setlocalvar("removing"+i, subent);
+                    }
+                }
+                hp = getentityproperty(p, "health");
+                if(getlocalvar("removing"+i) == NULL()) {
+                    if(hp > 0) {
+                        executeanimation(p, openborconstant("ANI_RUN"));
+                    }
+                    else {
+                        if(getlocalvar("player"+i+"die") == NULL()) {
+                            if(plives > 1) {
+                                executeanimation(p, openborconstant("ANI_FALL"));
+                                changeplayerproperty(i, "lives", plives-1);
+                                killentity(p);
+                                jumptobranch(openborvariant("current_branch"), 1);
+                                setlocalvar("player"+i+"die", 1);
+                            }
+                            else {
+                                executeanimation(p, openborconstant("ANI_FALL"));
+                                killentity(p);
+                                gameover();
+                                setlocalvar("player"+i+"die", 1);
+                            }
+                        }
+                    }
+                }
+                else {
+                    executeanimation(p, openborconstant("ANI_SPAWN"));
+                }
                 void iUp = playerkeys(i, 0, "moveup"); // New key status of "Up"
                 void iDown = playerkeys(i, 0, "movedown"); // New key status of "Down"
                 void iLeft = playerkeys(i, 0, "moveleft"); // New key status of "Left"
@@ -17,84 +79,80 @@ void main() {
                 void iDownR = playerkeys(i, 2, "movedown"); // New key status of "Down"
                 void iLeftR = playerkeys(i, 2, "moveleft"); // New key status of "Left"
                 void iRightR = playerkeys(i, 2, "moveright"); // New key status of "Right"
-
+                int Vxbase = 8;
+                int Vxback = 4;
                 int Vx, Vy, Vz;
-                Vx = 0;
+                Vx = Vxbase;
                 Vy = 0;
                 Vz = 0;
 
 
-                if(iRight) {
+                /*if(iRight) {
                     //changeentityproperty(p, "velocity", 1, 0, 0);
-                    Vx = 2;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
+                    Vx += 4;
                 }
                 if(iRightR) {
                     //changeentityproperty(p, "velocity", 0, 0, 0);
-                    Vx = 0;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
+                    Vx = Vxbase;
                 }
 
                 if(iLeft) {
                     //changeentityproperty(p, "velocity", -1, 0, 0);
-                    Vx =-1.5;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
+                    Vx = Vxback;
 
                 }
                 if(iLeftR) {
                     //changeentityproperty(p, "velocity", 0, 0, 0);
-                    Vx = 0;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
-                }
+                    Vx = Vxbase;
+                }*/
                 if(iUp){
                     //changeentityproperty(p, "velocity", 0, -2, 0);
                     Vy = -2;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
                 }
                 if(iUpR) {
                     //changeentityproperty(p, "velocity", 0, 0, 0);
                     Vy = 0;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
                 }
                 if(iDown){
                     ///changeentityproperty(p, "velocity", 0, 2, 0);
                     Vy = 2;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
                 }
                 if(iDownR) {
                     //changeentityproperty(p, "velocity", 0, 0, 0);
                     Vy = 0;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
                 }
                 /// do diagnal 
-                if(iDown && iRight) {
+                /*if(iDown && iRight) {
                     //changeentityproperty(p, "velocity", 1, 0.5, 0);
-                    Vx = 2;
+                    Vx += 4;
                     Vy = 1.5;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
                 }
 
                 if(iDown && iLeft) {
                     //changeentityproperty(p, "velocity", -1, 0.5, 0);
-                    Vx = -1.5;
+                    Vx = Vxback;
                     Vy = 1.5;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
                    
                 }
 
                 if(iUp && iRight){
                     //changeentityproperty(p, "velocity", 1, -0.5, 0);
-                    Vx = 2;
+                    Vx += 4;
                     Vy = -1.5;
-                    changeentityproperty(p, "velocity", Vx, Vy, Vz);
                     
                 }
 
                 if(iUp && iLeft){
                     //changeentityproperty(p, "velocity", -1, -0.5, 0);
-                    Vx = -1.5;
+                    Vx = Vxback;
                     Vy = -1.5;
+                }*/
+                if(getlocalvar("removing"+i) == NULL()) {
                     changeentityproperty(p, "velocity", Vx, Vy, Vz);
+                }
+                else {
+                    changeentityproperty(p, "velocity", 0, 0, 0);
+                    changeentityproperty(p, "speed", 0, 0, 0);
                 }
 
 
